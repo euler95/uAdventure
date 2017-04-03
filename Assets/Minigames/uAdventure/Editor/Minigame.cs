62fb8d4986767f60d69de3201942fe7210a5e435
euler95 <jasandov@ucm.es>
2017-04-03 20:21:52 +0200

    20:21

---
+++ Assets/Minigames/uAdventure/Editor/Minigame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using uAdventure.Core;
+
+namespace uAdventure.Minigame
+{
+	public class Minigame : HasId, Documented
+	{
+		public Minigame(string id)
+		{
+			Id = id;
+			Conditions = new Conditions();
+			Effects = new Effects();
+		}
+
+
+		public string Id { get; set; }
+
+		public string Documentation { get; set; }
+
+		public string Content { get; set; }
+
+		public Conditions Conditions { get; set; }
+		public Effects Effects { get; set; }
+
+		public string getDocumentation()
+		{
+			return Documentation;
+		}
+
+		public string getId()
+		{
+			return Id;
+		}
+
+		public void setDocumentation(string documentation)
+		{
+			Documentation = documentation;
+		}
+
+		public void setId(string id)
+		{
+			Id = id;
+		}
+	}
+}
--- Assets/Minigames/uAdventure/Editor/MinigamesWindowExtension.cs
+++ Assets/Minigames/uAdventure/Editor/MinigamesWindowExtension.cs
@@ -3,40 +3,101 @@ using System.Collections;
 using uAdventure.Core;
 using uAdventure.Editor;
 using UnityEditor;
+namespace uAdventure.Minigame
+{
+	[EditorWindowExtension(160, typeof(Minigame))]
+	public class MinigamesWindowsExtension : ReorderableListEditorWindowExtension {
 
-[EditorWindowExtension(160, typeof(MinigameEffect))]
-public class MinigamesWindowsExtension : DefaultButtonMenuEditorWindowExtension {
+		private Minigame selectedMinigame;
 
-	public MinigamesWindowsExtension(Rect rect, GUIStyle style, params GUILayoutOption[] options)
-		: base(rect, new GUIContent("Minijuegos"), style, options)
-	{
-		var content = new GUIContent();
+		public MinigamesWindowsExtension(Rect rect, GUIStyle style, params GUILayoutOption[] options)
+			: base(rect, new GUIContent("Minijuegos"), style, options)
+		{
+			var content = new GUIContent();
 
-		// Button
-		content.image = (Texture2D) Resources.Load("EAdventureData/img/icons/minigame", typeof(Texture2D));
-		content.text = "Minijuegos";
-		ButtonContent = content;
-	}
 
-	#region implemented abstract members of BaseWindow
-
-	public override void Draw (int aID)
-	{
-		EditorGUILayout.LabelField ("Hola minijuegos");
-		// Controller.getInstance ().getSelectedChapterDataControl ();
-	}
-
-	#endregion
-
-	#region implemented abstract members of ButtonMenuEditorWindowExtension
-
-	protected override void OnButton ()
-	{
-	}
-
-	#endregion
+			// Button
+			content.image = (Texture2D) Resources.Load("EAdventureData/img/icons/minigame", typeof(Texture2D));
+			content.text = "Minijuegos";
+			ButtonContent = content;
+		}
 
 
 
+		public override void Draw (int aID){
+			
+			if(selectedMinigame==null) {
+				EditorGUILayout.LabelField ("Select or create a new Minigame");
+				return;
+			// Controller.getInstance ().getSelectedChapterDataControl ();
+			}
+			/*
+			EditorGUILayout.BeginVertical ();
+			{
+				EditorGUI.BeginChangeCheck ();
+				selectedMinigame.Content = EditorGUILayout.DelayedTextField ("Content", selectedMinigame.Content);
+				//Initialize the first text field		
+			}
+			EditorGUILayout.EndVertical ();
+		*/
+		}
+			
+		protected override void OnElementNameChanged (UnityEditorInternal.ReorderableList r, int index, string newName)
+		{
+			Controller.getInstance().getSelectedChapterDataControl().getObjects<Minigame>()[index].Id = newName;
+		}
 
+		protected override void OnAdd (UnityEditorInternal.ReorderableList r)
+		{
+			Controller.getInstance().getSelectedChapterDataControl().getObjects<Minigame>().Add(new Minigame("Memorion"));
+		}
+
+		protected override void OnUpdateList (UnityEditorInternal.ReorderableList r)
+		{
+			r.list = Controller.getInstance().getSelectedChapterDataControl().getObjects<Minigame>().ConvertAll(minigame => minigame.Id);
+		}
+
+		protected override void OnAddOption (UnityEditorInternal.ReorderableList r, string option)
+		{}
+
+		protected override void OnSelect (UnityEditorInternal.ReorderableList r)
+		{
+			if(r.index == -1)
+			{
+				selectedMinigame = null;
+				return;
+			}
+
+			var newSelection = Controller.getInstance().getSelectedChapterDataControl().getObjects<Minigame>()[r.index];
+			if(newSelection != null && newSelection != selectedMinigame)
+			{
+				selectedMinigame = newSelection;
+				//RegenerateQR();
+			}
+		}
+		protected override void OnRemove (UnityEditorInternal.ReorderableList r)
+		{
+			Controller.getInstance().getSelectedChapterDataControl().getObjects<Minigame>().RemoveAt(r.index);
+		}
+		protected override void OnReorder (UnityEditorInternal.ReorderableList r)
+		{
+			string idToMove = r.list [r.index] as string;
+			var temp = Controller.getInstance ().getSelectedChapterDataControl ().getObjects<Minigame> ();
+			Minigame toMove = temp.Find (minigame => minigame.getId () == idToMove);
+			temp.Remove (toMove);
+			temp.Insert (r.index, toMove);
+		}
+
+
+
+		protected override void OnButton ()
+		{
+			selectedMinigame = null;
+			reorderableList.index = -1;
+		}
+
+
+
+
+		}
 }
